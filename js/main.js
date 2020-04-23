b07692a0e5e470a56d3803eb8222947eb13ac2a4
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-04-23 13:44:44 -0400

    working animated menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-
-
 function show() {
-    var food = ["appetizers", "ramen", "rice", "drinks", "dessert", "alcohol"];
+    var food = ["appetizers", "ramen", "rice", "drinks", "desserts", "alcohol"];
     var category = event.target.innerHTML.toLowerCase();
     for(var i = 0; i < 6; i++) {
         if(category == food[i]){
-            document.querySelector("." + category + "__row .menu__content").classList.toggle("hidden");
+            document.querySelector("." + category + "__row .menu__content").classList.toggle("no-height");
         }
     }
-    
 }
