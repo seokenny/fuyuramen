0a1220c55c5dd03663d736183c7bbe510ae48636
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-04-23 12:58:24 -0400

    adding menu.html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+function show() {
+    var food = ["appetizers", "ramen", "rice", "drinks", "dessert", "alcohol"];
+    var category = event.target.innerHTML.toLowerCase();
+    for(var i = 0; i < 6; i++) {
+        if(category == food[i]){
+            document.querySelector("." + category + "__row .menu__content").classList.toggle("hidden");
+        }
+    }
+    
+}
