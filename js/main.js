26261e69dd6f3d0596461a9272d7c46021008ced
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-04-23 14:42:29 -0400

    Working mobile nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+        $(this).toggleClass('open');
+        $(".mobile__nav").toggleClass("mobile__nav-show");
+    });
+    $(".mobile__nav ul li").click(function(){
+        $("#nav-icon3").toggleClass('open');
+        $(".mobile__nav").toggleClass("mobile__nav-show");
+    });
+});
+
 function show() {
     var food = ["appetizers", "ramen", "rice", "drinks", "desserts", "alcohol"];
     var category = event.target.innerHTML.toLowerCase();
@@ -6,4 +17,28 @@ function show() {
             document.querySelector("." + category + "__row .menu__content").classList.toggle("no-height");
         }
     }
+}
+
+function worklistScroll1() {
+    var goTo = $("#location");
+        var top = goTo.offset().top;
+        top-=100;
+        $('html,body').animate({scrollTop: top}, 'slow');
+        return false;
+}
+
+function worklistScroll2() {
+    var goTo = $("#aboutus");
+        var top = goTo.offset().top;
+        top-=100;
+        $('html,body').animate({scrollTop: top}, 'slow');
+        return false;
+}
+
+function home() {
+    var goTo = $(".contain");
+        var top = goTo.offset().top;
+        top-=0;
+        $('html,body').animate({scrollTop: top}, 'slow');
+        return false;
 }
